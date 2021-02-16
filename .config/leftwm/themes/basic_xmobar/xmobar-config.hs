b8c35d49f58ec8e9d8517b86c7a22e2af917505a
James Pogue <jamesipogue@protonmail.com>
2021-02-15 19:31:05 -0800

    updated leftwm

--- .config/leftwm/themes/basic_xmobar/xmobar-config.hs
+++ .config/leftwm/themes/basic_xmobar/xmobar-config.hs
@@ -1,6 +1,4 @@
-
-
-Config { font = "-misc-fixed-*-*-*-*-13-*-*-*-*-*-*-*"
+Config { font = "xft:Cozette:size=10:antialias=true"
     , bgColor = "black"
     , fgColor = "grey"
     , position = Top
@@ -8,16 +6,17 @@ Config { font = "-misc-fixed-*-*-*-*-13-*-*-*-*-*-*-*"
     , allDesktops = True
     , pickBroadest = False
     , overrideRedirect = False
-    , commands = [    Run Weather "KSHV" ["-t"," <tempF>F","-L","64","-H","77","--normal","green","--high","red","--low","lightblue"] 36000
-                    , Run Network "eno1" ["-L","0","-H","32","--normal","green","--high","red"] 10
+    , commands = [ -- Run Weather "KWARENTO258" ["-t"," <tempF>F","-L","64","-H","77","--normal","green","--high","red","--low","lightblue"] 36000
+                      Run Network "wlp3s0" ["-L","0","-H","32","--normal","green","--high","red"] 10
                     , Run MultiCpu ["-L","15","-H","50","--normal","green","--high","red"] 10
                     , Run Memory [] 10
                     , Run Swap [] 10
                     , Run TopProc [] 10
                     , Run Date "%a %b %_d %Y %H:%M:%S" "date" 10
                     , Run UnsafeStdinReader
+                    , Run Battery [] 10
     ]
     , sepChar = "%"
     , alignSep = "}{"
-    , template = "%UnsafeStdinReader% }{ %top% | %multicpu% | %memory% * %swap% | %eno1% | %date% | %KSHV%"
+    , template = "%UnsafeStdinReader% }{ %top% | %multicpu% | %memory% * %swap% | %battery% | %date%"
 }
